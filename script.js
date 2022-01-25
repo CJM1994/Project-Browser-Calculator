7ab483b8e03bf5b6345ca58de77ff53446b81d5b
CJM1994 <connorjohnmullin@gmail.com>
2022-01-25 08:59:39 -0800

    add minimum functionality  - all operations work

--- script.js
+++ script.js
@@ -22,3 +22,60 @@ const calculate = function (operator, x, y) {
     case '/': return divide(x, y);
   }
 }
+
+const displayInput = function (input) {
+  const display = document.querySelector('.calculator-display');
+  display.value = input;
+}
+
+const listener = function () {
+
+  let x = '';
+  let y = '';
+  let activeVariable = 'x';
+  let operator = '';
+  let result = 0;
+
+  const keys = document.querySelector('.calculator-keys');
+  keys.addEventListener('click', (event) => {
+    const keyClass = event.target.classList.value;
+    const keyValue = event.target.value;
+
+    if (keyClass === 'number' && activeVariable === 'x') {
+      x += keyValue;
+      displayInput(x);
+    }
+    else if (keyClass === 'number' && activeVariable === 'y') {
+      y += keyValue;
+      displayInput(y);
+    }
+
+    if (keyClass === 'operator' && activeVariable === 'x') {
+      activeVariable = 'y';
+      displayInput(keyValue);
+      operator = keyValue;
+    }
+
+    if (keyClass === 'equal-sign' && y !== '') {
+      result = calculate(operator, x, y);
+      displayInput(result);
+    }
+
+    if (keyClass === 'all-clear') {
+      x = '';
+      y = '';
+      activeVariable = 'x';
+      result = 0;
+      operator = '';
+      displayInput('0');
+    }
+
+  })
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed...')
+  console.log('Deploy listener for buttons & keys...')
+  listener();
+  console.log('Listener deployed...')
+})
